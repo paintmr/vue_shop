572bb6d851a8824224b890eaa7d118e6c2ef6558
chujingxl <chujingxl@gmail.com>
2021-09-14 23:29:31 +0800

    Vue全家桶-項目-Vue電商後台管理系統：3 登錄/退出功能（9）引入message彈框組件

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -6,20 +6,20 @@
         <img src="../assets/logo.png" alt="">
       </div>
       <!-- 登錄表單區域 -->
-        <el-form :model="loginForm" label-width="0px" class="login_form">
+        <el-form :model="loginForm" :rules="loginFormRules" ref="loginFormRef" label-width="0px" class="login_form">
           <!-- 用戶名 -->
-          <el-form-item>
+          <el-form-item prop="username">
             <el-input v-model="loginForm.username" prefix-icon="el-icon-user-solid"></el-input>
           </el-form-item>
           <!-- 密碼 -->
-          <el-form-item>
+          <el-form-item prop="password">
             <el-input v-model="loginForm.password" prefix-icon="el-icon-lock" type="password"></el-input>
           </el-form-item>
           <!-- 按鈕 -->
           <el-form-item>
               <div class="btns">
-                <el-button type="primary">登錄</el-button>
-                <el-button type="info">重置</el-button>
+                <el-button type="primary"  @click="loginValidate">登录</el-button>
+                <el-button type="info" @click="resetLoginForm">重置</el-button>
               </div>
           </el-form-item>
         </el-form>
@@ -28,14 +28,54 @@
 </template>
 
 <script>
+import { ElMessage } from 'element-plus'
+
 export default {
   data () {
     return {
       // 這是登錄表單的數據綁定對象
       loginForm: {
-        username: 'zs',
-        password: 'll'
+        username: 'zsa',
+        password: 'llaaaa'
+      },
+      // 表單的驗證規則對象
+      loginFormRules: {
+        // 驗證用戶名
+        username: [
+          { required: true, message: '请输入登錄名稱', trigger: 'blur' },
+          { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+        ],
+        // 驗證密碼
+        password: [
+          { required: true, message: '请输入密碼', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  methods: {
+    // 點擊重置按鈕，重置登錄表單
+    resetLoginForm () {
+      this.$refs.loginFormRef.resetFields()
+    },
+    // 點擊登錄按鈕，驗證表單數據
+    loginValidate () {
+      this.$refs.loginFormRef.validate(async valid => {
+        // 如果表单域验证成功，valid为true
+        if (!valid) return
+        const { data: res } = await this.$axios.post('login', this.loginForm)
+        if (res.meta.status !== 200) {
+          ElMessage.warning({
+            message: '登录失败',
+            type: 'warning'
+          })
+          return
+        }
+        ElMessage.success({
+          message: '登陆成功！',
+          type: 'success'
+        })
+      })
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import installElementPlus from './plugins/element'
 // 導入全局樣式表
 import './assets/css/global.css'
 
+// 引入axios
+import axios from 'axios'
 const app = createApp(App)
+
+// 全局配置axios，这样每个组件可以直接用
+app.config.globalProperties.$axios = axios
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+
 installElementPlus(app)
 app.use(router).mount('#app')
